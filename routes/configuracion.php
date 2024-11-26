f9e631f14a442c3e14248a3b1edf2dbc25d968fa
soportesaludtolimaips <soporte@saludtolimaips.com>
2024-11-26 17:58:45 -0500

    Update configuracion.php

--- routes/configuracion.php
+++ routes/configuracion.php
@@ -1,10 +1,13 @@
 <?php
 
 use App\Http\Controllers\Configuracion\ConfigDiviPoliController;
-use App\Models\Configuracion\ConfigDiviPoli;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->group(function () {
+
+    /**
+     * División política
+     */
     Route::resource('/divipoli', ConfigDiviPoliController::class)->except('create', 'edit');
     Route::get('/divipoli/list/paises', [ConfigDiviPoliController::class, 'paises'])->name('divipoli.list.paises');
     Route::get('/divipoli/list/departamentos/{paisId}', [ConfigDiviPoliController::class, 'departamento'])->name('divipoli.list.departamento');
