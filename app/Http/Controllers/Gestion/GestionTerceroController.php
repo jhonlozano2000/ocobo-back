01924515a45c9ab6805949ce1a7a0b3f2aea074c
soportesaludtolimaips <soporte@saludtolimaips.com>
2025-01-02 08:12:39 -0500

    Update GestionTerceroController.php

--- app/Http/Controllers/Gestion/GestionTerceroController.php
+++ app/Http/Controllers/Gestion/GestionTerceroController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Models\Gestion\GestionTercero;
 use \Validator;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class GestionTerceroController extends Controller
 {
