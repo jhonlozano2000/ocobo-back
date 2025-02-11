419e09253c2a978bf128cb59e76e9c181808e1bd
soportesaludtolimaips <soporte@saludtolimaips.com>
2025-02-11 16:30:54 -0500

    Modulo -> Clasificacion documental -> Importar TRD

--- routes/ventanilla.php
+++ routes/ventanilla.php
@@ -10,14 +10,14 @@ Route::middleware('auth:sanctum')->group(function () {
     /**
      * Correspondencia recibida
      */
-    Route::apiResource('radica-corres-recibida', VentanillaRadicaReciController::class)->except('create', 'edit');
-    Route::post('radica-corres-recibida/{id}/archivo', [VentanillaRadicaReciArchivosController::class, 'upload'])->name('radica.corres.recibida.upload');
-    Route::get('radica-corres-recibida/{id}/archivo', [VentanillaRadicaReciArchivosController::class, 'download'])->name('radica.corres.recibida.download');
-    Route::delete('radica-corres-recibida/{id}/archivo', [VentanillaRadicaReciArchivosController::class, 'deleteFile'])->name('radica.corres.recibida.deleteFile');
+    Route::apiResource('radica-recibida', VentanillaRadicaReciController::class)->except('create', 'edit');
+    Route::post('radica-recibida/{id}/archivo', [VentanillaRadicaReciArchivosController::class, 'upload'])->name('radica.corres.recibida.upload');
+    Route::get('radica-recibida/{id}/archivo', [VentanillaRadicaReciArchivosController::class, 'download'])->name('radica.corres.recibida.download');
+    Route::delete('radica-recibida/{id}/archivo', [VentanillaRadicaReciArchivosController::class, 'deleteFile'])->name('radica.corres.recibida.deleteFile');
 
     /**
      * Responsables
      */
-    Route::apiResource('radica-corres-recibida-responsables', VentanillaRadicaReciResponsaController::class)->except('create', 'edit');
-    Route::get('radica-corres-recibida-recibida/{id}/responsables', [VentanillaRadicaReciResponsaController::class, 'getByRadicado']);
+    Route::apiResource('radica-recibida-responsables', VentanillaRadicaReciResponsaController::class)->except('create', 'edit');
+    Route::get('radica-recibida-recibida/{id}/responsables', [VentanillaRadicaReciResponsaController::class, 'getByRadicado']);
 });
